fa1aa00ff910f288cae94447ed8953fc256efd10
Mika Tonteri <mikaton@protonmail.com>
2019-08-17 11:31:22 +0300

    tehtävä 1.7

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,35 +1,47 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  
+  const [all, setAll] = useState(0);
+  const [positive, setPositive] = useState(0);
+  const [average, setAverage] = useState(0);
+
   const handleGood = () => {
     setGood(good + 1);
+    setAll(all + 1);
   }
 
   const handleNeutral = () => {
     setNeutral(neutral + 1);
+    setAll(all + 1);
   }
 
   const handleBad = () => {
     setBad(bad + 1);
+    setAll(all + 1);
   }
+
+  useEffect(() => {
+    setAverage(good - bad / 3);
+    setPositive(good * 100 / all);
+  });
+
 
   return (
     <div>
       <h2>give feedback</h2>
-      <br />
       <button onClick={handleGood}>good</button>
       <button onClick={handleNeutral}>neutral</button>
       <button onClick={handleBad}>bad</button>
-      <br />
       <h2>statistics</h2>
-      <br />
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 
