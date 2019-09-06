5329824c4b1bc90066bc7a6ad3bf46210475fb3c
Mika Tonteri <mikaton@protonmail.com>
2019-09-06 14:42:19 +0300

    refaktoroitiin App-komponentti omaan tiedostoonsa

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -2,52 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import Course from './Course';
-
-const App = () => {
-    const courses = [
-        {
-            name: 'Half Stack application development',
-            parts: [
-                {
-                    name: 'Fundamentals of React',
-                    exercises: 10,
-                    id: 1
-                },
-            
-                {
-                    name: 'Using props to pass data',
-                    exercises: 7,
-                    id: 2
-                },
-            
-                {
-                    name: 'State of a component',
-                    exercises: 14,
-                    id: 3
-                }
-            ]
-        },
-        {
-            name: 'Testikurssi',
-            parts: [
-                {
-                    name: 'Testi yksi',
-                    exercises: 1,
-                    id: 4
-                }
-            ]
-        }
-    ];
-    
-    return (
-        <div>
-            {
-                courses.map(course => <Course course={course} />)
-            }
-        </div>
-    )
-}
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
