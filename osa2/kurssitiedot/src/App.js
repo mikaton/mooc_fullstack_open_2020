4ee243acb4d557ead09d4d00a909bcec0abeb2df
Mika Tonteri <mikaton@protonmail.com>
2019-09-13 11:50:29 +0300

    tehtävä 2.6

---
+++ osa2/kurssitiedot/src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import Course from './Course';
+
+const App = () => {
+    const courses = [
+        {
+            id: 1,
+            name: 'Half Stack application development',
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+            
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+            
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                }
+            ]
+        },
+        {
+            id: 2,
+            name: 'Testikurssi',
+            parts: [
+                {
+                    name: 'Testi yksi',
+                    exercises: 1,
+                    id: 4
+                }
+            ]
+        }
+    ];
+    
+    return (
+        <div>
+            {
+                courses.map(course => <Course key={course.id} course={course} />)
+            }
+        </div>
+    );
+}
+
+export default App;
---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+
+function App() {
+
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]);
+
+  const [newName, setNewName] = useState('');
+
+  const handleAddNewPerson = (event) => {
+    event.preventDefault();
+    const newPerson = { name: newName }
+    setPersons(persons.concat(newPerson));
+    setNewName('');
+  }
+
+  const handleInputChange = (event) => {
+    setNewName(event.target.value);
+  }
+
+  return (
+    <div className="App">
+      <h2>Phonebook</h2>
+      <form onSubmit={handleAddNewPerson}>
+        <div>
+          name: <input value={newName} onChange={handleInputChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {
+        persons.map(person => <p key={person.name}>{person.name}</p>)
+      }
+    </div>
+  );
+}
+
+export default App;
