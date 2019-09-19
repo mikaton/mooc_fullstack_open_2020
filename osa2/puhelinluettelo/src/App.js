0ba69c366a4da1841276674051f04a01f4e18be2
Mika Tonteri <mikaton@protonmail.com>
2019-09-19 09:28:36 +0300

    tehtävä 2.7

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
 
 function App() {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]);
+  const [newName, setNewName] = useState("");
 
-  const [newName, setNewName] = useState('');
-
-  const handleAddNewPerson = (event) => {
+  const handleAddNewPerson = event => {
     event.preventDefault();
-    const newPerson = { name: newName }
-    setPersons(persons.concat(newPerson));
-    setNewName('');
-  }
+    const newPerson = { name: newName };
+    if (persons.find(person => person.name === newName)) {
+      alert(`${newName} is already in the phonebook.`);
+    } else {
+      setPersons(persons.concat(newPerson));
+      setNewName("");
+    }
+  };
 
-  const handleInputChange = (event) => {
+  const handleInputChange = event => {
     setNewName(event.target.value);
-  }
+  };
 
   return (
     <div className="App">
@@ -32,9 +32,9 @@ function App() {
         </div>
       </form>
       <h2>Numbers</h2>
-      {
-        persons.map(person => <p key={person.name}>{person.name}</p>)
-      }
+      {persons.map(person => (
+        <p key={person.name}>{person.name}</p>
+      ))}
     </div>
   );
 }
