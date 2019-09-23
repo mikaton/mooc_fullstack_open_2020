08a7d783e03661c5f58761d7b0b471f65fee8a15
Mika Tonteri <mikaton@protonmail.com>
2019-09-23 15:14:02 +0300

    tehtävät 2.19 ja 2.20

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import FilterForm from "./FilterForm";
 import AddPersonForm from "./AddPersonForm";
 import PersonsList from "./PersonsList";
-
+import Message from "./Message";
 import * as personService from "./PersonService";
 
 function App() {
@@ -26,6 +26,9 @@ function App() {
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
 
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
   const handleAddNewPerson = event => {
     event.preventDefault();
 
@@ -38,18 +41,36 @@ function App() {
         )
       ) {
         const person = { name: personToFind.name, number: newNumber };
-        personService.update(personToFind.id, person).then(() => {
-          personService.getAll().then(persons => {
-            setPersons(persons);
-            setFilteredPersons(persons);
-            setNewName("");
-            setNewNumber("");
+        personService
+          .update(personToFind.id, person)
+          .then(() => {
+            setSuccessMessage(`Updated ${person.name}'s number`);
+            setTimeout(() => {
+              setSuccessMessage(null);
+            }, 3000);
+            personService.getAll().then(persons => {
+              setPersons(persons);
+              setFilteredPersons(persons);
+              setNewName("");
+              setNewNumber("");
+            });
+          })
+          .catch(() => {
+            setErrorMessage(
+              `${personToFind.name} on jo poistettu palvelimelta.`
+            );
+            setTimeout(() => {
+              setErrorMessage(null);
+            }, 5000);
           });
-        });
       }
     } else {
       const person = { name: newName, number: newNumber };
       personService.create(person).then(() => {
+        setSuccessMessage(`Added ${person.name}`);
+        setTimeout(() => {
+          setSuccessMessage(null);
+        }, 3000);
         setPersons(persons.concat(person));
         setNewName("");
         setNewNumber("");
@@ -63,10 +84,20 @@ function App() {
     if (window.confirm(`Poistetaanko ${personToDelete.name}?`)) {
       let newList = [];
       newList = persons.filter(person => person.id !== personToDelete.id);
-      personService.deleteById(personToDelete.id).then(() => {
-        setPersons(newList);
-        setFilteredPersons(newList);
-      });
+      personService
+        .deleteById(personToDelete.id)
+        .then(() => {
+          setPersons(newList);
+          setFilteredPersons(newList);
+        })
+        .catch(() => {
+          setErrorMessage(
+            `${personToDelete.name} on jo poistettu palvelimelta.`
+          );
+          setTimeout(() => {
+            setErrorMessage(null);
+          }, 5000);
+        });
     }
   };
 
@@ -99,6 +130,8 @@ function App() {
   return (
     <div className="App">
       <h2>Phonebook</h2>
+      <Message messageType="success" message={successMessage} />
+      <Message messageType="error" message={errorMessage} />
       <FilterForm handleFiltering={handleFiltering} />
       <h2>Add a new person</h2>
       <AddPersonForm
---
+++ osa2/puhelinluettelo/src/Message.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Message = ({ message, messageType }) => {
+  if (message === null) return null;
+
+  return <div className={messageType}>{message}</div>;
+};
+
+export default Message;
