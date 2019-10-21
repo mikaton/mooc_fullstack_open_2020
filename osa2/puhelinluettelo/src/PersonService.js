415229d89119593e609a63c85290e52ad646ea70
Mika Tonteri <mikaton@protonmail.com>
2019-10-21 09:40:09 +0300

    muutettu APIurl suhteelliseksi

--- osa2/puhelinluettelo/src/PersonService.js
+++ osa2/puhelinluettelo/src/PersonService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "https://fathomless-escarpment-15165.herokuapp.com/api/persons";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
   const req = axios.get(baseUrl);
