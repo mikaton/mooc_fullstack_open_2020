f695daf9fa4733ff29edbbd8fe1b435fd689bb69
Mika Tonteri <mikaton@protonmail.com>
2019-12-04 11:12:10 +0200

    tehtävä 4.8

--- osa4/app.js
+++ osa4/app.js
@@ -6,16 +6,17 @@ const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
 const middleware = require("./utils/middleware");
 const mongoose = require("mongoose");
+const logger = require("./utils/logger");
 
-console.log("Connecting to ", config.MONGODB_URI);
+logger.info("Connecting to ", config.MONGODB_URI);
 
 mongoose
   .connect(config.MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
-    console.log("Connected to MongoDB");
+    logger.info("Connected to MongoDB");
   })
   .catch(error => {
-    console.log("Error connecting to MongoDB: ", error.message);
+    logger.error("Error connecting to MongoDB: ", error.message);
   });
 
 app.use(cors());
