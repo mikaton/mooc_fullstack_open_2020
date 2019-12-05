e10185a1dc27afd102d672d0d9b5d69910f2ee60
Mika Tonteri <mikaton@protonmail.com>
2019-12-05 15:57:55 +0200

    Tehtävä 4.15

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -24,24 +24,24 @@ blogsRouter.post("/", async (request, response) => {
   response.status(201).json(savedBlog.toJSON());
 });
 
-blogsRouter.delete("/:id", async (request, response) => {
+blogsRouter.delete("/:id", async (request, response, next) => {
   const id = request.params.id;
   try {
     await Blog.findByIdAndDelete(id);
     response.status(200).json({ message: "Blog deleted successfully" });
   } catch {
-    return response.status(404).json({ error: "blog not found" });
+    return response.status(404).json({ error: "Blog not found" });
   }
 });
 
-blogsRouter.patch("/:id", async (request, response) => {
+blogsRouter.patch("/:id", async (request, response, next) => {
   const body = request.body;
 
   try {
     await Blog.findByIdAndUpdate(request.params.id, body);
     response.status(200).json({ message: "blog updated successfully" });
-  } catch (err) {
-    response.status(404).json({ error: err });
+  } catch {
+    return response.status(404).json({ error: "Blog not found" });
   }
 });
 
---
+++ osa4/controllers/users.js
@@ -0,0 +1,27 @@
+const bcrypt = require("bcryptjs");
+const usersRouter = require("express").Router();
+const User = require("../models/user");
+
+usersRouter.get("/", async (request, response, next) => {
+  const users = await User.find({});
+  response.json(users.map(user => user.toJSON()));
+});
+
+usersRouter.post("/", async (request, response, next) => {
+  try {
+    const body = request.body;
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(body.password, saltRounds);
+    const user = new User({
+      username: body.username,
+      name: body.name,
+      passwordHash,
+    });
+    const savedUser = await user.save();
+    response.json(savedUser);
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = usersRouter;
