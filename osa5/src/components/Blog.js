7d90b2fe3420678d8504cfd314cfb61d40b96ef2
Mika Tonteri <mikaton@protonmail.com>
2019-12-18 13:32:18 +0200

    tehtävä 5.6

--- osa5/src/components/Blog.js
+++ osa5/src/components/Blog.js
@@ -1,8 +1,36 @@
-import React from "react";
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import React, { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [showBlogDetails, setShowBlogDetails] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const blogDetails = () => (
+    <div onClick={() => setShowBlogDetails(false)}>
+      <p>{blog.title}</p>
+      <p>{blog.url}</p>
+      <p>{blog.likes} likes</p>
+      <p>added by {blog.user.name}</p>
+      <button onClick={() => console.log(blog)}>like</button>
+    </div>
+  );
+
+  return (
+    <div style={blogStyle}>
+      {showBlogDetails && blogDetails()}
+      {!showBlogDetails && (
+        <div onClick={() => setShowBlogDetails(true)}>
+          {blog.title} {blog.author}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
