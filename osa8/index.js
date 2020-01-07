865856c3c4b6f916922ddbedd61d2b8dfc028db3
Mika Tonteri <mikaton@protonmail.com>
2020-01-07 11:13:28 +0200

    tehtävä 8.5

--- osa8/index.js
+++ osa8/index.js
@@ -101,7 +101,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -111,9 +111,17 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (args.author === undefined) {
+      if (!args.author && !args.genre) {
         return books;
+      } else if (args.author && args.genre) {
+        return books.filter(
+          book =>
+            book.author === args.author && book.genres.includes(args.genre)
+        );
+      } else if (!args.author && args.genre) {
+        return books.filter(book => book.genres.includes(args.genre));
       }
+
       return books.filter(book => book.author === args.author);
     },
     allAuthors: () =>
