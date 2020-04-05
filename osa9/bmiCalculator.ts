9e0b820595fcb2556a954c4b144f0fbb5a842f4e
Mika Tonteri <mikaton@protonmail.com>
2020-04-05 12:51:11 +0300

    ex 9.1

---
+++ osa9/bmiCalculator.ts
@@ -0,0 +1,16 @@
+const calculateBmi = (weight: number, height: number): string => {
+  let heightInMeters = height / 100;
+  const bmi: number = weight / (heightInMeters ** 2);
+  
+  if(bmi < 18.5) {
+    return "Underweight"
+  } else if(bmi >= 18.5 && bmi <= 25) {
+    return "Normal (healthy weight)"
+  } else if(bmi > 25 && bmi <= 30) {
+    return "Overweight"
+  } else if(bmi > 30) {
+    return "Obese"
+  }
+}
+
+console.log(calculateBmi(115, 175));
