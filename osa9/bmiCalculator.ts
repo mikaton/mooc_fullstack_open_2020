878f7209db32bfb3e5ad3edac7f60b9fbee4a397
Mika Tonteri <mikaton@protonmail.com>
2020-04-05 12:52:52 +0300

    fixed comparison error (ex. 9.1)

--- osa9/bmiCalculator.ts
+++ osa9/bmiCalculator.ts
@@ -4,9 +4,9 @@ const calculateBmi = (weight: number, height: number): string => {
   
   if(bmi < 18.5) {
     return "Underweight"
-  } else if(bmi >= 18.5 && bmi <= 25) {
+  } else if(bmi >= 18.5 && bmi < 25) {
     return "Normal (healthy weight)"
-  } else if(bmi > 25 && bmi <= 30) {
+  } else if(bmi > 25 && bmi < 30) {
     return "Overweight"
   } else if(bmi > 30) {
     return "Obese"
