a88da9d27de5a92af31e0dc0f655a5f546946d42
Mika Tonteri <mikaton@protonmail.com>
2020-04-05 12:27:22 +0300

    .

---
+++ osa7/blogilista/src/reducers/userReducer.js
@@ -0,0 +1,24 @@
+import userService from '../services/users';
+
+const userReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'INITIALIZE_USERS':
+      return action.data;
+    case 'SET_USER':
+
+    default:
+      return state;
+  }
+};
+
+export const initUsers = () => {
+  return async dispatch => {
+    const users = await userService.getAll();
+    dispatch({
+      type: 'INITIALIZE_USERS',
+      data: users,
+    });
+  };
+};
+
+export default userReducer;
